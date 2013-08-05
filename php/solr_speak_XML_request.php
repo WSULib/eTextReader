a9ffc01f29d3f5be1115065b866182d134e70213
ghukill <ghukill@gmail.com>
2013-08-05 09:44:29 -0400

    fixed speaking AGAIN

--- php/solr_speak_XML_request.php
+++ php/solr_speak_XML_request.php
@@ -7,35 +7,4 @@ $datastream_bits = file_get_contents($squery);
 echo $datastream_bits;
 return;
 
-// // XML
-// if ($data_type == 'xml'){
-// 	// returns XML object - this might be more appropriate for content metadata
-// 	$xml_simple = simplexml_load_file($request_URL);	
-// 	// print_r($xml_simple);
-// 	$json_simple = json_encode($xml_simple);
-// 	// print_r($json_simple);
-// 	echo $json_simple;
-// 	return;
-// }
-
-// //JSON
-// elseif ($data_type == 'json') {
-// 	//returns file contents untouched
-// 	$datastream_bits = file_get_contents($request_URL);	
-// 	echo $datastream_bits;
-// 	return;
-// }
-
-// HTML
-// else {
-// 	//returns file contents untouched
-// 	$datastream_bits = file_get_contents($request_URL);	
-// 	echo $datastream_bits;
-// 	return;
-// }
-
-
-
-
-
 ?>
